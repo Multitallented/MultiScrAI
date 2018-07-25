aadc8352323cf6d4e5bd5eef5b35255a67ebeef9
Dan Miller <dan.miller@verys.com>
2018-07-25 16:11:33 -0700

    stripped out the bot logic for a fresh template

--- src/main/java/com/nicktoony/scrAI/Constants.java
+++ src/main/java/com/nicktoony/scrAI/Constants.java
@@ -10,48 +10,6 @@ import org.stjs.javascript.annotation.GlobalScope;
  */
 @GlobalScope
 public class Constants {
-    public static int TIER_LOW = 1;
-    public static int TIER_MEDIUM = 2;
-    public static int TIER_HIGH = 3;
-
-    public static int ALERT_STATUS_CRITICAL = 1; // 100% military, 0% economy
-    public static int ALERT_STATUS_HIGH = 2; //
-    public static int ALERT_STATUS_MEDIUM = 3; // 50% military, 50% ecconomy
-    public static int ALERT_STATUS_LOW = 4; //
-    public static int ALERT_STATUS_NONE = 5; // 0% military, 100% economy
-
-    public static int OFFSET_ROOM_STORAGE = 100; // useful to stop the AI spending too much early on
-    public static int DELAY_RANDOM = 20;
-    public static int DELAY_PATH_MANAGER = 50; // how many ticks between checking paths are okay
-    public static int DELAY_ENERGY_MANAGER = 20;
-    public static int DELAY_ADVISORS = 15;
-    public static int DELAY_CONSTRUCTION_MANAGER = 50;
-    public static int DELAY_STRUCTURE_MANAGER = 100;
-    public static int DELAY_POPULATION_MANAGER = 150;
-    public static int DELAY_SPAWN_MANAGER = 100;
-    public static int DELAY_TASK_MANAGER = 100;
-    public static int DELAY_SOURCES_MANAGER = 100;
-
-    public static int PRIORITY_BUILD = 25;
-    public static int PRIORITY_UPGRADE = 0;
-    public static int PRIORITY_PICKUP = 100;
-    public static int PRIORITY_DEPOSIT = 25;
-    public static int PRIORITY_REPAIR = 50;
-
-    // Creep ids
-    public static String CREEP_MINER_ID = "1";
-    public static String CREEP_MINER_NAME = "CreepMiner";
-    public static String CREEP_WORKER_ID = "2";
-    public static String CREEP_WORKER_NAME = "CreepWorker";
-
-    // Creep settings (miner)
-    public static int SETTING_MINER_PER_SPAWN = 2;
-    public static int SETTING_MAX_ROAD_CREATE = 1;
-    public static int SETTINGS_PATH_REUSE = 50;
-    public static int SETTINGS_PATH_EXPIRE_TIME = 200;
-
-    // Source constants
-    public static final int SOURCE_REGEN = 300;
 
     // Creep part costs
     public static final int MOVE_COST = 50;
--- src/main/java/com/nicktoony/scrAI/main.java
+++ src/main/java/com/nicktoony/scrAI/main.java
@@ -1,6 +1,6 @@
 package com.nicktoony.scrAI;
 
-import com.nicktoony.scrAI.Controllers.GlobalController;
+import com.nicktoony.scrAI.Controllers.GlobalManager;
 import com.nicktoony.screeps.Game;
 import org.stjs.javascript.Global;
 
@@ -8,11 +8,11 @@ import org.stjs.javascript.Global;
  * Created by nick on 26/07/15.
  *
  *  var stjs = require("stjs");
- *  var GlobalController = require("GlobalController");
+ *  var GlobalManager = require("GlobalManager");
  */
 public class main {
     public static void main(String[] args) {
-        new GlobalController();
+        new GlobalManager();
         Global.console.log("Tick Finished, used CPU: " + Game.cpu.getUsed());
     }
 }
